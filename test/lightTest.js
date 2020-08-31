af92af8bdcfdafb2acced6b998037067e33f140f
Nina Köck <koeckni@eworxs-Air.loxone.local>
2020-08-31 13:58:05 +0200

    update lightTest

--- test/lightTest.js
+++ test/lightTest.js
@@ -3,7 +3,7 @@ module.exports = {
   doTest: function doTest(socket) {
     var scene = 4
     
-      return socket.send("jdev/sps/io/15064d77-002f-de7c-ffffc1a0bc6dbf48/changeTo/" + scene).then(function(respons) {
+      return socket.send("jdev/sps/io/15064d77-002f-de7c-ffffc1a0bc6dbf48/chngeTo/" + scene).then(function(respons) {
         return respons.LL
         }, function (e) {
           throw console.error("socket send failed");
