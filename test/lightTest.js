1a3c282a01eba6d0b058baa84884fdc2a43019eb
Nina Köck <koeckni@eworxs-Air.loxone.local>
2020-08-31 10:22:18 +0200

    lightTest change scene

--- test/lightTest.js
+++ test/lightTest.js
@@ -1,7 +1,7 @@
 //Export code to test
 module.exports = { 
   doTest: function doTest(socket) {
-    var scene = 778
+    var scene = 2
     
       return socket.send("jdev/sps/io/15064d77-002f-de7c-ffffc1a0bc6dbf48/changeTo/" + scene).then(function(respons) {
           return respons.LL
